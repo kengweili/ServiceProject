bcc3c3eabb8870c16638ce3d353a3001a12f3cac
Keng Wei <kengwei@example.com>
2025-12-12 11:36:46 +0000

    add 911 -t app

---
+++ 911/aspnet-core/src/ServiceProject.DbMigrator/ServiceProjectDbMigratorModule.cs
@@ -0,0 +1,14 @@
+﻿using ServiceProject.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace ServiceProject.DbMigrator;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(ServiceProjectEntityFrameworkCoreModule),
+    typeof(ServiceProjectApplicationContractsModule)
+    )]
+public class ServiceProjectDbMigratorModule : AbpModule
+{
+}
---
+++ 911/aspnet-core/src/ServiceProject.HttpApi.Host/ServiceProjectBrandingProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Localization;
+using ServiceProject.Localization;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
+
+namespace ServiceProject;
+
+[Dependency(ReplaceServices = true)]
+public class ServiceProjectBrandingProvider : DefaultBrandingProvider
+{
+    private IStringLocalizer<ServiceProjectResource> _localizer;
+
+    public ServiceProjectBrandingProvider(IStringLocalizer<ServiceProjectResource> localizer)
+    {
+        _localizer = localizer;
+    }
+
+    public override string AppName => _localizer["AppName"];
+}
